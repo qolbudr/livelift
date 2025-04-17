e4ee5d160286f159d6206dca26f4d82d9d5f148a
Qolbu Dzikru <qolbu.dzikru@protonmail.com>
2025-04-17 20:33:59 +0700

    fix-add timestamp package

--- prisma/migrations/20250417133328_create_users_and_packages_table/migration.sql
+++ prisma/migrations/20250417133328_create_users_and_packages_table/migration.sql
@@ -9,6 +9,8 @@ CREATE TABLE `Packages` (
     `custom_logo` BOOLEAN NOT NULL,
     `scheduling` BOOLEAN NOT NULL,
     `price` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
