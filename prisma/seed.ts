98fd01b1ca6608e49779e96b6ced6393a375662c
Qolbu Dzikru <qolbu.dzikru@protonmail.com>
2025-04-22 21:47:43 +0700

    fix-package seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,18 +11,18 @@ async function main() {
   await prisma.$queryRaw`TRUNCATE Users`;
   await prisma.$queryRaw`TRUNCATE Packages`;
 
-  const password = await bcrypt.hash('11223344', 8);
+  // const password = await bcrypt.hash('11223344', 8);
 
   for (let i = 0; i < packageData.length; i++) {
     let element = packageData[i];
     await prisma.packages.create({ data: element });
   }
 
-  for (let i = 0; i < userData.length; i++) {
-    let element = userData[i];
-    element.password = password;
-    await prisma.users.create({ data: element });
-  }
+  // for (let i = 0; i < userData.length; i++) {
+  //   let element = userData[i];
+  //   element.password = password;
+  //   await prisma.users.create({ data: element });
+  // }
 }
 
 main()
