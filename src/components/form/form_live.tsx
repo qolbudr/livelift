3f939274c05394eb0e7be5514b884a7ed2eb92ec
Qolbu Dzikru <qolbu.dzikru@protonmail.com>
2025-04-23 14:10:22 +0700

    fix-add scheduling

--- src/components/form/form_live.tsx
+++ src/components/form/form_live.tsx
@@ -24,7 +24,7 @@ export const FormLive = ({ item, getLive }: { item: Live, getLive: () => void })
   const user = useUser();
 
   useEffect(() => {
-    if (user && user!.phone.scheduling) {
+    if (user && user!.package.scheduling) {
       setSchedule(item.scheduleAt ? true : false);
     } else {
       setSchedule(false);
